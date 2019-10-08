49e7aba7101834e92260e36df50b1b386c91934a
Gabriel Piagentini <gmtpiagentini@gmail.com>
2019-10-08 14:30:48 -0300

    Finish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,11 @@ import { SuccessPanelComponent } from './Components/success-panel/success-panel.
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
+import { ConfirmationPanelComponent } from './Components/questions-panel/confirmation-panel/confirmation-panel.component';
+import { InsertPanelComponent } from './Components/questions-panel/insert-panel/insert-panel.component';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,15 +21,20 @@ import {MatButtonModule} from '@angular/material/button';
     WelcomePanelComponent,
     QuestionsPanelComponent,
     MainFormComponent,
-    SuccessPanelComponent
+    SuccessPanelComponent,
+    ConfirmationPanelComponent,
+    InsertPanelComponent,
   ],
   imports: [
     BrowserModule,
     FontAwesomeModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
